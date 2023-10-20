fe7601d8737e3b5fe2acf73570b0dacc3b5ce90e
Zahid Hussain <69339367+zeestack@users.noreply.github.com>
2023-10-20 14:54:09 -0600

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+});
